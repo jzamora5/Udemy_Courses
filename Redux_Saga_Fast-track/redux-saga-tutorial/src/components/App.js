3c4e2983079baf387d51d8200c251bcf7a43a311
jzamora5 <jzamora_5@yahoo.com>
2021-09-30 20:13:34 -0500

    Start creating sagas and generators

--- Redux_Saga_Fast-track/redux-saga-tutorial/src/components/App.js
+++ Redux_Saga_Fast-track/redux-saga-tutorial/src/components/App.js
@@ -1,5 +1,23 @@
-function App() {
-  return <div>Test</div>;
+import React, { Component } from 'react';
+
+function* testing() {
+  while (true) {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+}
+
+class App extends Component {
+  render() {
+    const iterator = testing();
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+
+    return <div>test</div>;
+  }
 }
 
 export default App;
