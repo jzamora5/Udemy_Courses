61719cca15ee4730c617fcd9f0e1b488f9c6a7fb
jzamora5 <jzamora_5@yahoo.com>
2021-10-04 19:46:32 -0500

    Connect saga middleware to app

--- Redux_Saga_Fast-track/redux-saga-tutorial/src/components/App.js
+++ Redux_Saga_Fast-track/redux-saga-tutorial/src/components/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getUsersRequest } from '../actions/users';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props.getUsersRequest();
+  }
+
   render() {
     return <div>test</div>;
   }
 }
 
-export default App;
+export default connect(null, {
+  getUsersRequest,
+})(App);
