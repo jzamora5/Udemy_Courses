15515257232cbaf39952411b377c35cf5d0ecd6a
jzamora5 <jzamora_5@yahoo.com>
2021-10-05 19:01:06 -0500

    Bulding the UI

--- Redux_Saga_Fast-track/redux-saga-tutorial/src/components/App.js
+++ Redux_Saga_Fast-track/redux-saga-tutorial/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUsersRequest } from '../actions/users';
+import UsersList from './UsersList';
+import NewUserForm from './NewUserForm';
 
 class App extends Component {
   constructor(props) {
@@ -9,11 +11,21 @@ class App extends Component {
     this.props.getUsersRequest();
   }
 
+  handleSubmit = ({ firstName, lastName }) => {
+    console.log(firstName, lastName);
+  };
+
   render() {
-    return <div>test</div>;
+    const users = this.props.users;
+    return (
+      <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
+        <NewUserForm onSubmit={this.handleSubmit} />
+        <UsersList users={users.items} />
+      </div>
+    );
   }
 }
 
-export default connect(null, {
+export default connect(({ users }) => ({ users }), {
   getUsersRequest,
 })(App);
