037372558ff2744c5234858f493e64234a80577a
jzamora5 <jzamora_5@yahoo.com>
2021-10-08 19:52:55 -0500

    Delete Saga

--- Redux_Saga_Fast-track/redux-saga-tutorial/src/components/App.js
+++ Redux_Saga_Fast-track/redux-saga-tutorial/src/components/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getUsersRequest, createUserRequest } from '../actions/users';
+import {
+  getUsersRequest,
+  createUserRequest,
+  deleteUserRequest,
+} from '../actions/users';
 import UsersList from './UsersList';
 import NewUserForm from './NewUserForm';
 
@@ -18,12 +22,19 @@ class App extends Component {
     });
   };
 
+  handleDeleteUserClick = (userId) => {
+    this.props.deleteUserRequest(userId);
+  };
+
   render() {
     const users = this.props.users;
     return (
       <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
         <NewUserForm onSubmit={this.handleSubmit} />
-        <UsersList users={users.items} />
+        <UsersList
+          users={users.items}
+          onDeleteUser={this.handleDeleteUserClick}
+        />
       </div>
     );
   }
@@ -32,4 +43,5 @@ class App extends Component {
 export default connect(({ users }) => ({ users }), {
   getUsersRequest,
   createUserRequest,
+  deleteUserRequest,
 })(App);
